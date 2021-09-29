0ad6a2c5fe05d9f8b68d92deda60a2e3a57abf8b
Joseph-GM <jkim.english75@gmail.com>
2021-09-29 13:08:39 +0900

    until 2:13, about button

--- app/src/main/java/org/joetech/mylibrary/MainActivity.java
+++ app/src/main/java/org/joetech/mylibrary/MainActivity.java
@@ -1,10 +1,12 @@
 package org.joetech.mylibrary;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -56,6 +58,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, FavoriteActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(getString(R.string.app_name));
+                builder.setMessage("Desinged and Developed with Love by Mesiam at meiCod.ort\n" + "Check my website for more awesome applications");
+                builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //TODO:
+                    }
+                });
+                builder.setPositiveButton("Visit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(MainActivity.this, WebsiteActivity.class);
+                        intent.putExtra("url", "https://google.com");
+                        startActivity(intent);
+                    }
+                });
+
+
+                builder.create().show();
+
+
+
             }
         });
 
